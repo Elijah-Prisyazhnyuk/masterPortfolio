1e7a9e70b67b90c6ccbd0deb468525a2ab107274
Elijah Prisiazhniuk <118561968+Elijah-Prisyazhnyuk@users.noreply.github.com>
2024-02-06 12:20:49 -0800

    Update SkillSection.js

--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -3,20 +3,12 @@ import "./Skills.css";
 import SoftwareSkill from "../../components/softwareSkills/SoftwareSkill";
 import { skills } from "../../portfolio";
 import { Fade } from "react-reveal";
-import DataScienceImg from "./DataScienceImg";
-import FullStackImg from "./FullStackImg";
-import CloudInfraImg from "./CloudInfraImg";
-import DesignImg from "./DesignImg";
+import EmbeddedEngineerImg from "./fullstack.svg"
+import DataScienceImg from "./data_science.svg";
+import CloudInfraImg from "./cloud_infrastructure.svg";
+import DesignImg from "./ui_ux_design.svg";
 
-function GetSkillSvg(props) {
-  if (props.fileName === "DataScienceImg")
-    return <DataScienceImg theme={props.theme} />;
-  else if (props.fileName === "FullStackImg")
-    return <FullStackImg theme={props.theme} />;
-  else if (props.fileName === "CloudInfraImg")
-    return <CloudInfraImg theme={props.theme} />;
-  return <DesignImg theme={props.theme} />;
-}
+
 
 class SkillSection extends Component {
   render() {
@@ -28,11 +20,10 @@ class SkillSection extends Component {
             <div key={i} className="skills-main-div">
               <Fade left duration={2000}>
                 <div className="skills-image-div">
-                  {/* <img
+                  <img
                     alt="Ashutosh is Analysing Data"
                     src={require(`../../assests/images/${skill.imagePath}`)}
-                  ></img> */}
-                  <GetSkillSvg fileName={skill.fileName} theme={theme} />
+                  ></img>
                 </div>
               </Fade>
 

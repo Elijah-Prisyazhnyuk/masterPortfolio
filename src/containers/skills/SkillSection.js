12d73f00b39b6063e89d23c0461deb5b871b38eb
Elijah Prisiazhniuk <118561968+Elijah-Prisyazhnyuk@users.noreply.github.com>
2024-02-06 12:30:16 -0800

    Update SkillSection.js

--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -22,7 +22,7 @@ class SkillSection extends Component {
                 <div className="skills-image-div">
                   <img
                     alt="Ashutosh is Analysing Data"
-                    src={require({skill.imagePath})
+                    src={require({skill})
                   ></img>
                 </div>
               </Fade>

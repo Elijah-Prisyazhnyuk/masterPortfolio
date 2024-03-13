5f0942813771f477e8b307c114b72b57cfeb7d25
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-13 10:54:24 -0700

    another fix

--- src/portfolio.js
+++ src/portfolio.js
@@ -221,6 +221,38 @@ const competitiveSites = {
       profileLink: "https://leetcode.com/layman_brother/",
     },
     {
+      siteName: "HackerRank",
+      iconifyClassname: "simple-icons:hackerrank",
+      style: {
+        color: "#2EC866",
+      },
+      profileLink: "https://www.hackerrank.com/layman_brother",
+    },
+    {
+      siteName: "Codechef",
+      iconifyClassname: "simple-icons:codechef",
+      style: {
+        color: "#5B4638",
+      },
+      profileLink: "https://www.codechef.com/users/ashutosh_1919",
+    },
+    {
+      siteName: "Codeforces",
+      iconifyClassname: "simple-icons:codeforces",
+      style: {
+        color: "#1F8ACB",
+      },
+      profileLink: "http://codeforces.com/profile/layman_brother",
+    },
+    {
+      siteName: "Hackerearth",
+      iconifyClassname: "simple-icons:hackerearth",
+      style: {
+        color: "#323754",
+      },
+      profileLink: "https://www.hackerearth.com/@ashutosh391",
+    },
+    {
       siteName: "Kaggle",
       iconifyClassname: "simple-icons:kaggle",
       style: {
@@ -349,9 +381,48 @@ const experience = {
   title: "Experience",
   subtitle: "Internship and Freelance",
   description:
-    "I have experience of working with different data sources from online marketplaces and social networks. Junior Data Analyst with strong Electrical Engineer background. Prefer a data mining, visualization and creating specialized AI tools. I have experience of working for a large industrial company such as EVRAZ as a design engineer. I also have experience in academic freelance assignments related to MatLAB, IoT, Proteus.",
+    "I have worked with many evolving startups as ML and DL Developer, Designer and Software Architect. I have also worked with some well established companies mostly as AI Developer. I love organising events and that is why I am also involved with many opensource communities as a representative.",
   header_image_path: "experience.svg",
   sections: [
+    {
+      title: "Work",
+      work: true,
+      experiences: [
+        {
+          title: "Associate AI Engineer",
+          company: "Legato Health Technology",
+          company_url: "https://legatohealthtech.com/",
+          logo_path: "legato_logo.png",
+          duration: "June 2020 - Aug 2021",
+          location: "Hyderabad, Telangana",
+          description:
+            "I am working on automating healthcare products. The projects involve automation for process improvements and for significantly enhancing the profits. I am currently working on Cancer Survival and Reoccurence Prediction. Our goal is to make AI system which scales and removes doctor dependency as much as possible.",
+          color: "#0879bf",
+        },
+        {
+          title: "Android and ML Developer",
+          company: "Muffito Incorporation",
+          company_url: "https://www.linkedin.com/company/muffito-inc/about/",
+          logo_path: "muffito_logo.png",
+          duration: "May 2018 - Oct 2018",
+          location: "Pune, Maharashtra",
+          description:
+            "I have created complete Android Application for locating Pub, Bar and beverage shops around you. I have also worked on implementation of algorithms for Face Detection, Text extraction from Image. I was involved in a team for creating complete software architecure of mobile and web application as well as admin panel for company.",
+          color: "#9b1578",
+        },
+        {
+          title: "Android Developer",
+          company: "FreeCopy Pvt. Ltd.",
+          company_url: "https://www.linkedin.com/company/freecopy/about/",
+          logo_path: "freecopy_logo.png",
+          duration: "Nov 2017 - Dec 2017",
+          location: "Ahmedabad, Gujarat",
+          description:
+            "FreeCopy is the Start up from Indian Institute of Management, Ahmedabad. I have changed the integration of the whole app from Google to Firebase. I learnt the efﬁcient ways of Data communications like Retroﬁt, Eventbus etc. I experienced the real time start up. I learnt the Design thinking of UI on perspective of People.",
+          color: "#fc1f20",
+        },
+      ],
+    },
     {
       title: "Internships",
       experiences: [
@@ -392,7 +463,7 @@ const experience = {
       ],
     },
     {
-      title: "Freelance",
+      title: "Volunteerships",
       experiences: [
         {
           title: "Google Explore ML Facilitator",
@@ -519,16 +590,16 @@ const publications = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "",
+    profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours.",
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
   addressSection: {
     title: "Address",
     subtitle:
       "Ambavadi vas, Kanodar, T.A.-Palanpur, Dist.-Banaskantha, Gujarat - 385520",
-    locality: "Chelyabinsk",
-    country: "RU",
+    locality: "Kanodar",
+    country: "IN",
     region: "Gujarat",
     postalCode: "385520",
     streetAddress: "Ambavadi vas",

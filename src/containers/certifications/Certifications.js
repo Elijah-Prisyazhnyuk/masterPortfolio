34390c048038519b9aecb51c0a92fd660326e95b
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-19 06:15:59 -0700

    oof

--- src/containers/certifications/Certifications.js
+++ src/containers/certifications/Certifications.js
@@ -1,28 +1,31 @@
+// Certifications.js
 import React from "react";
 import "./Certifications.css";
 import { Fade } from "react-reveal";
 import { certifications } from "../../portfolio";
-import { useLanguage } from "../../components/header/LanguageSelector";
+import { LanguageContext } from "../../components/header/LanguageSelector";
 import CertificationCard from "../../components/certificationCard/CertificationCard";
 
 const Certifications = ({ theme }) => {
-  const { currentLang } = useLanguage();
-
   return (
-    <div className="main" id="certs">
-      <div className="certs-header-div">
-        <Fade bottom duration={2000} distance="20px">
-          <h1 className="certs-header" style={{ color: theme.text }}>
-            Awards
-          </h1>
-        </Fade>
-      </div>
-      <div className="certs-body-div">
-        {certifications.certifications.map((cert) => {
-          return <CertificationCard key={cert.id} certificate={cert} lang={currentLang} theme={theme} />;
-        })}
-      </div>
-    </div>
+    <LanguageContext.Consumer>
+      {({ currentLang }) => (
+        <div className="main" id="certs">
+          <div className="certs-header-div">
+            <Fade bottom duration={2000} distance="20px">
+              <h1 className="certs-header" style={{ color: theme.text }}>
+                Awards
+              </h1>
+            </Fade>
+          </div>
+          <div className="certs-body-div">
+            {certifications.certifications.map((cert) => {
+              return <CertificationCard key={cert.id} certificate={cert} lang={currentLang} theme={theme} />;
+            })}
+          </div>
+        </div>
+      )}
+    </LanguageContext.Consumer>
   );
 };
 

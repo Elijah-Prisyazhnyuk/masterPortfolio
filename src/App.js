a4e5b369a2e202fbb052d8e68cc2e7bc0cb11db4
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-19 05:31:34 -0700

    c

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
+import { LanguageProvider } from './components/header/LanguageSelector';
 
 function App() {
   return (
     <ThemeProvider theme={chosenTheme}>
+      <LanguageProvider>
+      
       <>
         <GlobalStyles />
         <div>
           <Main theme={chosenTheme}/>
         </div>
       </>
+      </LanguageProvider>
     </ThemeProvider>
   );
 }

0f64f544d1331e4ed5ca18ea46339bb1331b9dd5
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-20 04:45:49 -0700

    a

--- src/pages/experience/Experience.js
+++ src/pages/experience/Experience.js
@@ -147,6 +147,7 @@ import ExperienceImg from "./ExperienceImg";
 class Experience extends Component {
   render() {
     const theme = this.props.theme;
+    console.log("props ", this.props)
     console.log("lg exp: ", this.props.lang);
     return (
       <div className="experience-main">

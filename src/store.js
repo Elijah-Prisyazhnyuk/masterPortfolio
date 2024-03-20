1b053121f9d58328bc3f72e0027da47c8037f075
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-20 00:28:49 -0700

    q2

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from 'redux';
-import rootReducer from './reducer';
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducer'
 
-const store = configureStore(rootReducer);
+const store = configureStore({ reducer: rootReducer })
 
 export default store;

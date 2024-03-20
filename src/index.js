3a3d53e882586ad873b2e2f292f5cca7474eea44
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-20 00:19:58 -0700

    3

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BaseProvider, LightTheme } from "baseui";
 import { Provider as StyletronProvider } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as ReduxProvider } from 'react-redux'; // Import Redux Provider
+import store from './store'; // Import your Redux store
 
 import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,7 +17,9 @@ const engine = new Styletron();
 ReactDOM.render(
   <StyletronProvider value={engine}>
     <BaseProvider theme={LightTheme}>
-      <App />
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
     </BaseProvider>
   </StyletronProvider>,
   document.getElementById("root")
@@ -24,4 +28,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();

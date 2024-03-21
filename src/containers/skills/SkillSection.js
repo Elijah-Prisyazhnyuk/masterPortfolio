af78f70e16633acbf664d5d012cd8385dd9cfffb
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-21 01:40:35 -0700

    i

--- src/containers/skills/SkillSection.js
+++ src/containers/skills/SkillSection.js
@@ -37,8 +37,9 @@ class SkillSection extends Component {
                       className="skills-heading"
                       style={{ color: theme.text }}
                     >
-                      {skill['title_${lang}']}
+                      {skill[`{"title_"${lang}}`]}
                     </h1>
+                    {console.log("testtest ", `{"title_"${lang}}`) }
                     <br/>
                     {/*<h1
                       id={lang === 'ru' ? 'ru' : 'hide'}
@@ -84,9 +85,9 @@ class SkillSection extends Component {
                       className="skills-heading"
                       style={{ color: theme.text }}
                     >
-                      {skill['title_${lang}']}
+                      {skill[`{"title_"${lang}}`]}
                     </h1>
-                    {console.log("testtest ",['title_${lang}'])}
+                    {console.log("testtest ", `{"title_"${lang}}`) }
                     <br/>
                     {/*<h1
                       id={lang === 'ru' ? 'ru' : 'hide'}

4c298d069dd5dd7cb5397ea640f1514f0eb8ff6a
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-21 04:41:49 -0700

    fix

--- src/components/publicationsCard/PublicationCard.js
+++ src/components/publicationsCard/PublicationCard.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./PublicationCard.css";
 import { Fade } from "react-reveal";
 
-export default function PublicationCard({ pub, theme }) {
+export default function PublicationCard({ lang, pub, theme }) {
   function openPubinNewTab(url) {
     var win = window.open(url, "_blank");
     win.focus();
   }
-  const lang = this.props.lang;
 
   return (
     <div

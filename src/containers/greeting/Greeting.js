2503af3f4e3cf88c95f0fb7f53166f7478d97c4e
Elijah-Prisyazhnyuk <ilya_prisyazhnyuk@mail.ru>
2024-03-21 14:06:39 -0700

    ss

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -11,13 +11,11 @@ export default function Greeting(props) {
   const theme = props.theme;
   useEffect(() => {
     const avatar = document.getElementById('avatar');
-    const gloss = avatar.querySelector('.card__gloss');
     const width = window.innerWidth;
     const height = window.innerHeight;
 
     const init = () => {
       bindMouse();
-      light(350, -125);
       tilt(350, -125);
     };
 
@@ -28,14 +26,9 @@ export default function Greeting(props) {
     const handleMouseMove = (event) => {
       const x = event.clientX - (width / 2);
       const y = event.clientY - (height / 2);
-      light(x, y);
       tilt(x, y);
     };
 
-    const light = (x, y) => {
-      const angle = (Math.atan2(y, x) * 180) / Math.PI - 90;
-      gloss.style.background = `linear-gradient(${angle}deg, rgba(255, 255, 255, ${y / height * .9}) 0%, rgba(255, 255, 255, 0) 80%)`;
-    };
 
     const tilt = (x, y) => {
       const force = 80;
@@ -78,9 +71,7 @@ export default function Greeting(props) {
             <img className="avatar"
 							alt="Yes, it's me"
 							src={require(`../../assests/images/portfolio.png`)}
-              >
-                <div className="card__gloss"/>
-              </img>
+              />
             <div className="button-greeting-div">
               <Button text={greeting.btn_contact[lang]} theme={theme} href="contact" />
               <Button text={greeting.btn_resume[lang]} theme={theme} newTab={true} href={greeting.resumeLink} />
